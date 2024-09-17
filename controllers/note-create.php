d60cc6d4652e4105f70a40e5484a2daf138ab1ef
mohanadsyria40 <mohanadsyria40@gmal.com>
2024-09-17 11:09:32 +0300

    Create the first validation layer for notes creation.

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -6,8 +6,16 @@ $config = require('config.php');
 $db = new Database($config['database']);
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
-    $db->query("INSERT INTO notes(body, user_id) VALUES(:body, :user_id)", ['body' => $_POST['body'],
-        'user_id' => 1] );
+    $errors = [];
+    if(strlen($_POST['body']) == 0){
+        $errors['body'] = "Please enter a note.";
+    }
+
+    if(empty($errors['body'])){
+        $db->query("INSERT INTO notes(body, user_id) VALUES(:body, :user_id)", ['body' => $_POST['body'],
+            'user_id' => 1] );
+    }
+
 }
 
 require "views/note-create.view.php";
