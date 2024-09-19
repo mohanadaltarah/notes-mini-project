8395b0d5150c4cc131eeb9df3ef526dc73656543
mohanadsyria40 <mohanadsyria40@gmal.com>
2024-09-19 09:35:40 +0300

    Separate requests for specific controllers.

--- controllers/notes/destroy.php
+++ controllers/notes/destroy.php
@@ -13,10 +13,11 @@ $note  = $db->query("SELECT * FROM notes WHERE id = :id", ['id' => $_GET['id']
 
 authorize($note['user_id'] == $currentUser);
 
-view("notes/show.view.php", [
-    "heading" => "Note",
-    "note" => $note
-]);
+$db->query("delete from notes where id = :id", [
+    "id" => $_POST["id"]]
+);
 
+header("Location: /notes");
+exit();
 
 
