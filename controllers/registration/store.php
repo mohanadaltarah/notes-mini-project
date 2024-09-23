024b72293d009f3fa3e5659805c46ee7f2e05841
mohanadsyria40 <mohanadsyria40@gmal.com>
2024-09-23 14:43:09 +0300

    Create hashing for the password.

--- controllers/registration/store.php
+++ controllers/registration/store.php
@@ -29,7 +29,7 @@ if ($user) {
 } else {
     $db->query('INSERT INTO users(email, password) VALUES(:email, :password)', [
         'email' => $email,
-        'password' => $password // NEVER store database passwords in clear text. We'll fix this in the login form episode. :)
+        'password' => password_hash($password, PASSWORD_DEFAULT) // NEVER store database passwords in clear text. We'll fix this in the login form episode. :)
     ]);
 
     $_SESSION['user'] = [
